3f1c32feb893b9a3c15f6527399268d80581b761
chpark11 <pcm3164@naver.com>
2022-02-09 17:02:17 +0900

    login

--- Project/Project/App/views.py
+++ Project/Project/App/views.py
@@ -1,17 +1,65 @@
 from contextlib import redirect_stderr
 from django.shortcuts import render, redirect
-from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password, check_password
+from django.shortcuts import get_object_or_404
 from .models import Ingredienttbl
 from .models import Usertbl
 from .models import Recipetbl
-# Create your views here.
 
 def login(request):
-    return render(request, 'webApp/login.html')
+    if request.method == 'GET':
+        return render(request, 'webApp/login.html')
+    elif request.method == 'POST':
+        userID = request.POST.get('userid', None)
+        userpassword = request.POST.get('userpassword', None)
+        res_data = {}
+        if not (userID and userpassword):
+            res_data['error'] = '모든 값을 입력해주세요.'
+            #모든 값이 작성되지 않을시 에러표시
+        else:
+            Users = get_object_or_404(Usertbl,userid=userID)
+            #페이지에서 받아온 userID값과 같은 userid값을 가진 객체를 db의 Usertbl에서 불러와 User객체에 저장.같은 값이 없으면 404에러 호출 
+            if check_password(userpassword, Users.userpassword):
+            #페이지에서 받아온 비밀번호와 User객체의 비밀번호가 일치한지 검사
+                user_code = Users.usercode
+                #User객체의 usercode(pk)를 user_code에 할당
+                request.session['user'] = user_code
+                #user를 키로하고 값을 user_code로 하는 세션 설정
+                return render(request, 'webApp/mainlist.html', {'Users':Users})
+            else:
+                res_data['error'] = '잘못된 아이디 또는 비밀번호입니다.'
+        return render(request, 'webApp/login.html', res_data)
+                
+def register(request):  
+    if request.method == 'GET':
+        return render(request, 'webApp/register.html')
+    elif request.method == 'POST':
+        username = request.POST.get('username', None)
+        useremail = request.POST.get('useremail', None)
+        userID = request.POST.get('userID', None)
+        password = request.POST.get('password', None)   
+        passwordconfirm = request.POST.get('passwordconfirm', None)           
 
+        res_data = {}
+        
+        if password != passwordconfirm:
+            res_data['error'] = '비밀번호가 일치하지 않습니다.'
+        else:
+            user = Usertbl(
+                username=username,
+                useremail=useremail,
+                userid=userID,
+                userpassword=make_password(password)
+            )
+            user.save()
+        
+        return render(request, 'webApp/register.html', res_data)
+        
 def logout(request):
-    logout(request)
-    return render('webApp/login.html')
+    if request.session.get('user'):
+        del request.session['user']
+    #세션이 존재하면 세션을 제거하여 로그아웃
+    return redirect('/')
 
 def save(request):
     return render(request, 'webApp/save.html')
@@ -33,6 +81,6 @@ def mainlist(request):
 def listsave(request):
     return render(request, 'webApp/listsave.html')
 
-def register(request):  
-    return render(request, 'webApp/register.html')
+def listview(request):  
+    return render(request, 'webApp/listview.html')
     
