e2d6c469373b1581aa43857e1037285cc7b9c1b1
chpark11 <pcm3164@naver.com>
2022-02-09 18:11:56 +0900

    login2

--- Project/Project/App/views.py
+++ Project/Project/App/views.py
@@ -2,6 +2,7 @@ from contextlib import redirect_stderr
 from django.shortcuts import render, redirect
 from django.contrib.auth.hashers import make_password, check_password
 from django.shortcuts import get_object_or_404
+from django.http import HttpResponse
 from .models import Ingredienttbl
 from .models import Usertbl
 from .models import Recipetbl
@@ -17,15 +18,15 @@ def login(request):
             res_data['error'] = '모든 값을 입력해주세요.'
             #모든 값이 작성되지 않을시 에러표시
         else:
-            Users = get_object_or_404(Usertbl,userid=userID)
+            User = get_object_or_404(Usertbl,userid=userID)
             #페이지에서 받아온 userID값과 같은 userid값을 가진 객체를 db의 Usertbl에서 불러와 User객체에 저장.같은 값이 없으면 404에러 호출 
-            if check_password(userpassword, Users.userpassword):
+            if check_password(userpassword, User.userpassword):
             #페이지에서 받아온 비밀번호와 User객체의 비밀번호가 일치한지 검사
-                user_code = Users.usercode
+                user_code = User.usercode
                 #User객체의 usercode(pk)를 user_code에 할당
                 request.session['user'] = user_code
                 #user를 키로하고 값을 user_code로 하는 세션 설정
-                return render(request, 'webApp/mainlist.html', {'Users':Users})
+                return redirect('mainlist')
             else:
                 res_data['error'] = '잘못된 아이디 또는 비밀번호입니다.'
         return render(request, 'webApp/login.html', res_data)
@@ -74,7 +75,7 @@ def search(request):
 def Userlist(request):
     return render(request, 'webApp/Userlist.html')
 
-def mainlist(request):
+def mainlist(request):    
     Recipes = Recipetbl.objects.all()
     return render(request, 'webApp/mainlist.html', {'Recipes':Recipes})
 
