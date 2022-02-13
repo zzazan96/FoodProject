36d6c5b12e44e1e5e9f193dcfb12e4d4c5d3f3c3
chpark11 <pcm3164@naver.com>
2022-02-14 02:48:17 +0900

    view

--- Project/Project/App/views.py
+++ Project/Project/App/views.py
@@ -5,6 +5,9 @@ from django.shortcuts import get_object_or_404
 from .models import Ingredienttbl
 from .models import Usertbl
 from .models import Recipetbl
+from .models import Listtbl
+from .models import Recipeingredienttbl
+
 
 def login(request):
     if request.method == 'GET':
@@ -18,7 +21,7 @@ def login(request):
         else:
             User = get_object_or_404(Usertbl,userid=userID)
             if check_password(userpassword, User.userpassword):
-                user_code = User.userid
+                user_code = User.usercode
                 request.session['user'] = user_code
                 return redirect('mainlist')
             else:
@@ -56,8 +59,10 @@ def logout(request):
     return redirect('/')
 
 def save(request):
-    return render(request, 'webApp/save.html')
-
+    Lists = Listtbl.objects.all()
+    return render(request, 'webApp/save.html', {'Lists': Lists})
+    
+        
 def savefix(request):
     return render(request, 'webApp/savefix.html')
 
@@ -75,12 +80,24 @@ def mainlist(request):
 def listsave(request):
     return render(request, 'webApp/listsave.html')
 
-def list(request):  
-    return render(request, 'webApp/list.html')
+def list(request):
+    if request.method == 'GET':
+        return render(request, 'webApp/list.html')
+    elif request.method == 'POST':
+        recipeID = request.POST.get('recipeid', None)
+        print(recipeID)
+        Recipe = get_object_or_404(Recipetbl,recipeid=recipeID)
+        data = {
+            'recipeid': Recipe.recipeid,
+            'recipedetail': Recipe.recipedetail,
+            'recipename': Recipe.recipename
+        }        
+        return render(request, 'webApp/list.html', data)
 
 def list2(request):  
     return render(request, 'webApp/list2.html')
 
-def cook(request):  
-    return render(request, 'webApp/cook.html')
+def cook(request):
+    Recipes = Recipetbl.objects.all()  
+    return render(request, 'webApp/cook.html', {'Recipes':Recipes})
     
